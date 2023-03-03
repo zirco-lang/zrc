514a442ef553bab1f0b8176f0bb381cdaa66f472
0xLogN <nutdriver716@gmail.com>
2023-03-03 08:32:01 -0800

    refactor: use subenum

--- src/ast.rs
+++ src/ast.rs
@@ -1,53 +1,192 @@
+#![allow(ambiguous_associated_items)]
+use subenum::subenum;
+
+#[subenum(
+    Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+    Term, Factor, Primary, IDENTIFIER
+)]
 #[derive(PartialEq, Debug, Clone, new)]
 pub enum Expr {
-    Comma(Box<Expr>, Box<Expr>),
-    Assignment(Box<Expr>, Box<Expr>),
-    AdditionAssignment(Box<Expr>, Box<Expr>),
-    SubtractionAssignment(Box<Expr>, Box<Expr>),
-    MultiplicationAssignment(Box<Expr>, Box<Expr>),
-    DivisionAssignment(Box<Expr>, Box<Expr>),
-    ModuloAssignment(Box<Expr>, Box<Expr>),
-    BitwiseAndAssignment(Box<Expr>, Box<Expr>),
-    BitwiseOrAssignment(Box<Expr>, Box<Expr>),
-    BitwiseXorAssignment(Box<Expr>, Box<Expr>),
-    BitwiseLeftShiftAssignment(Box<Expr>, Box<Expr>),
-    BitwiseRightShiftAssignment(Box<Expr>, Box<Expr>),
-    UnaryNot(Box<Expr>),
-    UnaryMinus(Box<Expr>),
-    PreIncrement(Box<Expr>),
-    PreDecrement(Box<Expr>),
-    Index(Box<Expr>, Box<Expr>),
-    Dot(Box<Expr>, String),
-    NamespaceAccess(Box<Expr>, String),
-    PostIncrement(Box<Expr>),
-    PostDecrement(Box<Expr>),
-    Call(Box<Expr>, Vec<Expr>),
-    Ternary(Box<Expr>, Box<Expr>, Box<Expr>),
-    LogicalAnd(Box<Expr>, Box<Expr>),
-    LogicalOr(Box<Expr>, Box<Expr>),
-    Equals(Box<Expr>, Box<Expr>),
-    NotEquals(Box<Expr>, Box<Expr>),
-    BitwiseAnd(Box<Expr>, Box<Expr>),
-    BitwiseOr(Box<Expr>, Box<Expr>),
-    BitwiseXor(Box<Expr>, Box<Expr>),
-    GreaterThan(Box<Expr>, Box<Expr>),
-    GreaterThanOrEqualTo(Box<Expr>, Box<Expr>),
-    LessThan(Box<Expr>, Box<Expr>),
-    LessThanOrEqualTo(Box<Expr>, Box<Expr>),
-    BitwiseRightShift(Box<Expr>, Box<Expr>),
-    BitwiseLeftShift(Box<Expr>, Box<Expr>),
-    Addition(Box<Expr>, Box<Expr>),
-    Subtraction(Box<Expr>, Box<Expr>),
-    Multiplication(Box<Expr>, Box<Expr>),
-    Division(Box<Expr>, Box<Expr>),
-    Modulo(Box<Expr>, Box<Expr>),
+    #[subenum(Comma)]
+    Comma(Box<Comma>, Box<Assignment>),
 
+    #[subenum(Comma, Assignment)]
+    Assignment(Box<Unary>, Box<Assignment>),
+    #[subenum(Comma, Assignment)]
+    AdditionAssignment(Box<Unary>, Box<Assignment>),
+    #[subenum(Comma, Assignment)]
+    SubtractionAssignment(Box<Unary>, Box<Assignment>),
+    #[subenum(Comma, Assignment)]
+    MultiplicationAssignment(Box<Unary>, Box<Assignment>),
+    #[subenum(Comma, Assignment)]
+    DivisionAssignment(Box<Unary>, Box<Assignment>),
+    #[subenum(Comma, Assignment)]
+    ModuloAssignment(Box<Unary>, Box<Assignment>),
+    #[subenum(Comma, Assignment)]
+    BitwiseAndAssignment(Box<Unary>, Box<Assignment>),
+    #[subenum(Comma, Assignment)]
+    BitwiseOrAssignment(Box<Unary>, Box<Assignment>),
+    #[subenum(Comma, Assignment)]
+    BitwiseXorAssignment(Box<Unary>, Box<Assignment>),
+    #[subenum(Comma, Assignment)]
+    BitwiseLeftShiftAssignment(Box<Unary>, Box<Assignment>),
+    #[subenum(Comma, Assignment)]
+    BitwiseRightShiftAssignment(Box<Unary>, Box<Assignment>),
+
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    UnaryNot(Box<Unary>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    UnaryMinus(Box<Unary>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    PreIncrement(Box<Unary>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    PreDecrement(Box<Unary>),
+
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    Index(Box<Postfix>, Box<Expr>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    Dot(Box<Postfix>, Box<IDENTIFIER>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    NamespaceAccess(Box<Postfix>, Box<IDENTIFIER>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    PostIncrement(Box<Postfix>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    PostDecrement(Box<Postfix>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    Call(Box<Postfix>, Vec<Assignment>),
+
+    #[subenum(Comma, Assignment, Unary, Postfix, Ternary)]
+    Ternary(Box<Logical>, Box<Expr>, Box<Ternary>),
+
+    #[subenum(Comma, Assignment, Unary, Postfix, Ternary, Logical)]
+    LogicalAnd(Box<Logical>, Box<Equality>),
+    #[subenum(Comma, Assignment, Unary, Postfix, Ternary, Logical)]
+    LogicalOr(Box<Logical>, Box<Equality>),
+
+    #[subenum(Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality)]
+    Equals(Box<Equality>, Box<Bitwise>),
+    #[subenum(Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality)]
+    NotEquals(Box<Equality>, Box<Bitwise>),
+
+    #[subenum(Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise)]
+    BitwiseAnd(Box<Bitwise>, Box<Comparison>),
+    #[subenum(Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise)]
+    BitwiseOr(Box<Bitwise>, Box<Comparison>),
+    #[subenum(Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise)]
+    BitwiseXor(Box<Bitwise>, Box<Comparison>),
+
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison
+    )]
+    GreaterThan(Box<Comparison>, Box<Shift>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison
+    )]
+    GreaterThanOrEqualTo(Box<Comparison>, Box<Shift>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison
+    )]
+    LessThan(Box<Comparison>, Box<Shift>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison
+    )]
+    LessThanOrEqualTo(Box<Comparison>, Box<Shift>),
+
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift
+    )]
+    BitwiseRightShift(Box<Shift>, Box<Term>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift
+    )]
+    BitwiseLeftShift(Box<Shift>, Box<Term>),
+
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term
+    )]
+    Addition(Box<Term>, Box<Factor>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term
+    )]
+    Subtraction(Box<Term>, Box<Factor>),
+
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    Multiplication(Box<Factor>, Box<Unary>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    Division(Box<Factor>, Box<Unary>),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor
+    )]
+    Modulo(Box<Factor>, Box<Unary>),
+
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor, Primary
+    )]
     NumberLiteral(String),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor, Primary
+    )]
     StringLiteral(String),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor, Primary, IDENTIFIER
+    )]
     Identifier(String),
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor, Primary
+    )]
     BooleanLiteral(bool),
-
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor, Primary
+    )]
     Parenthesized(Box<Expr>),
 
+    #[subenum(
+        Comma, Assignment, Unary, Postfix, Ternary, Logical, Equality, Bitwise, Comparison, Shift,
+        Term, Factor, Primary
+    )]
     Error,
 }
--- src/parser_tests.rs
+++ src/parser_tests.rs
@@ -18,11 +18,11 @@ mod tests {
             result,
             Ok(box_arguments!(
                 Expr::new_addition,
-                Expr::new_number_literal("1".to_string()),
+                crate::ast::Term::new_number_literal("1".to_string()),
                 box_arguments!(
-                    Expr::new_multiplication,
-                    Expr::new_number_literal("2".to_string()),
-                    Expr::new_number_literal("3".to_string())
+                    crate::ast::Factor::new_multiplication,
+                    crate::ast::Factor::new_number_literal("2".to_string()),
+                    crate::ast::Unary::new_number_literal("3".to_string())
                 )
             ))
         );
@@ -38,8 +38,8 @@ mod tests {
             result,
             Ok(box_arguments!(
                 Expr::new_addition,
-                Expr::new_number_literal("1".to_string()),
-                Expr::Error
+                crate::ast::Term::new_number_literal("1".to_string()),
+                crate::ast::Factor::new_error()
             ))
         );
 
@@ -50,5 +50,24 @@ mod tests {
             panic!("Unexpected error type");
         }
         assert_eq!(errors[0].dropped_tokens, vec![]);
+    }
+
+    #[test]
+    fn namespace_test() {
+        let mut errors = Vec::new();
+        let input = "io::println(\"hi\")";
+        let result = parser::ExprParser::new().parse(&mut errors, lexer::ZircoLexer::new(input));
+        assert_eq!(
+            result,
+            Ok(Expr::new_call(
+                Box::new(box_arguments!(
+                    crate::ast::Postfix::new_namespace_access,
+                    crate::ast::Postfix::new_identifier("io".to_string()),
+                    crate::ast::IDENTIFIER::new_identifier("println".to_string())
+                )),
+                vec![crate::ast::Assignment::StringLiteral("\"hi\"".to_string())]
+            ))
+        );
+        assert_eq!(errors, vec![]);
     }
 }
