6adbaf50ef89eebfa9c4bba253632aa01b1a6a68
0xLogN <logn@0xlogn.dev>
2023-09-14 15:54:52 -0700

    replicate 3b392a6

--- compiler/zrc_hir/src/lib.rs
+++ compiler/zrc_hir/src/lib.rs
@@ -1,2 +1,11 @@
+#![warn(
+    clippy::cargo,
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::missing_docs_in_private_items,
+    missing_docs
+)]
+#![allow(clippy::multiple_crate_versions)]
+
 pub mod hir;
 pub mod lower_ast;
