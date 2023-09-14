3b392a6a98bb9b10d309287a65bbeba34930c9f1
0xLogN <logn@0xlogn.dev>
2023-09-14 15:53:55 -0700

    add new clippy rules

--- compiler/zrc/src/main.rs
+++ compiler/zrc/src/main.rs
@@ -1,3 +1,12 @@
+#![warn(
+    clippy::cargo,
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::missing_docs_in_private_items,
+    missing_docs
+)]
+#![allow(clippy::multiple_crate_versions)]
+
 use zrc_parser::parser;
 
 fn main() {
--- compiler/zrc_parser/src/lib.rs
+++ compiler/zrc_parser/src/lib.rs
@@ -1,3 +1,12 @@
+#![warn(
+    clippy::cargo,
+    clippy::nursery,
+    clippy::pedantic,
+    clippy::missing_docs_in_private_items,
+    missing_docs
+)]
+#![allow(clippy::multiple_crate_versions)]
+
 #[macro_use]
 extern crate lalrpop_util;
 
