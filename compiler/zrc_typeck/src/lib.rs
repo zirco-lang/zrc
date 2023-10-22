fc3287ba779893c1893c8745b47d76f84ee354f8
0xLogN <logn@0xlogn.dev>
2023-10-22 14:10:14 -0700

    enable a bunch of lints

--- compiler/zrc_typeck/src/lib.rs
+++ compiler/zrc_typeck/src/lib.rs
@@ -3,12 +3,44 @@
 //! This crate contains functions for generating the [Typed AST](tast) and check
 //! program types in the process.
 
+#![allow(unknown_lints)] // in case you use non-nightly clippy
 #![warn(
     clippy::cargo,
     clippy::nursery,
     clippy::pedantic,
     clippy::missing_docs_in_private_items,
-    missing_docs
+    missing_docs,
+    clippy::absolute_paths,
+    clippy::as_conversions,
+    clippy::dbg_macro,
+    clippy::decimal_literal_representation,
+    clippy::default_numeric_fallback,
+    clippy::deref_by_slicing,
+    clippy::disallowed_script_idents,
+    clippy::else_if_without_else,
+    clippy::empty_structs_with_brackets,
+    clippy::format_push_string,
+    clippy::if_then_some_else_none,
+    clippy::let_underscore_must_use,
+    clippy::min_ident_chars,
+    clippy::mixed_read_write_in_expression,
+    clippy::multiple_inherent_impl,
+    clippy::multiple_unsafe_ops_per_block,
+    clippy::non_ascii_literal,
+    clippy::pattern_type_mismatch,
+    clippy::redundant_type_annotations,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::same_name_method,
+    clippy::semicolon_inside_block,
+    clippy::unseparated_literal_suffix,
+    clippy::string_to_string,
+    clippy::todo,
+    clippy::undocumented_unsafe_blocks,
+    clippy::unimplemented,
+    clippy::unneeded_field_pattern,
+    clippy::wildcard_enum_match_arm,
+    clippy::print_stderr, // These should be enabled in any user-facing code but forbidden in things like the parser
+    clippy::print_stdout  // Ditto
 )]
 #![allow(clippy::multiple_crate_versions, clippy::cargo_common_metadata)]
 
