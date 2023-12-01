97373fb6cfa46aa48ce56ae205fef4a488e4494d
0xLogN <logn@0xlogn.dev>
2023-12-01 09:17:07 -0800

    change comment

--- compiler/zrc_codegen/src/test_utils.rs
+++ compiler/zrc_codegen/src/test_utils.rs
@@ -3,52 +3,20 @@
 //! Only available on crate feature `test`.
 //!
 //! # Common patterns in tests
-//! Because we use `inkwell` to generate LLVM IR, it is often not possible to
-//! directly compare IRs with each other in tests. For this reason, testing the
-//! code generator can be quite difficult and there are a few common patterns we
-//! use to make these tests simpler and more consistent with each other. If a
-//! test does not follow these paradigms, you will usually find a comment
-//! explaining why.
+//! The old form of unit tests for `zrc_codegen` involved directly invoking the functions we are
+//! testing, which meant we needed to prepare all of the needed scope for inkwell and this led to
+//! very complicated and long tests (with most averaging at least 100 lines). This deterred me from
+//! testing as often as I wanted, and many bugs were not found because of this.
 //!
-//! First of all, because all LLVM structures borrow from the [`Context`]
-//! instance, every function will begin with the initialization of a
-//! [`Context`]. This one context belongs to a single test, and there should
-//! almost never exist two contexts within one test. This one context will
-//! generate two [`Module`]s, both named `test`, which we will explore in more
-//! detail in a few paragraphs.
+//! Instead, we now make our unit tests entire *programs* that we compile through the same process
+//! that the frontend uses, and we snapshot test the result. However, this almost turns out to be
+//! an integration test, and not a unit test! The reason I still consider these to be unit tests
+//! is because the code is short and meant to invoke a very specific block of code in whatever
+//! function we are testing, and not a full program. We will have full integration tests in the
+//! future.
 //!
-//! Every test also contains a "prelude." The prelude is a closure returned from
-//! [`make_test_prelude_closure`] which takes the [`Context`], calls
-//! [`initialize_test_function`] to obtain the required structures, and then
-//! calls a closure passed to its argument that will allow you to perform any
-//! needed test setup.
-//!
-//! This function may be used to generate the state of code generation before
-//! your test, such as any needed allocations. It can also mutate the scope and
-//! do any other needed setup.
-//!
-//! This will then be followed by two blocks statements bound to `expected` and
-//! `actual`. These will both call the prelude function to obtain the initial
-//! state, and then perform some mutations. These blocks can then return
-//! whatever values are to be compared, usually as a tuple with the first
-//! element being `module.print_to_string()` and the second being something like
-//! the return value of the function we are testing.
-//!
-//! **Keep in mind that the items being compared should NEVER contain LLVM
-//! structures.** They should at the most consist of
-//! [`inkwell::support::LLVMString`]s, because all LLVM structures borrow from
-//! the [`Module`] along with the [`Context`] and will segfault if the
-//! [`Module`] is dropped and then you run a comparison on them. You must print
-//! them to strings.
-//!
-//! It has been decided this test architecture is cleanest for allowing us to
-//! test the equality of IRs.
-//!
-//! tl;dr: Context is created, prelude will initialize both expected/actual IRs,
-//! and then we compare the stringified value of both.
-//!
-//! For a simple test example, read
-//! [`crate::expr::tests::cg_place::identifier_registers_are_returned_as_is`].
+//! Read any test for an example of how they flow. We recommend `cargo-insta` be installed so you
+//! can `cargo insta review` any changed snapshots.
 
 use inkwell::{
     basic_block::BasicBlock,
@@ -61,10 +29,10 @@ use inkwell::{
 };
 use zrc_typeck::{
     tast::{stmt::ArgumentDeclarationList, ty::Type},
-    typeck::{self, BlockReturnType},
+    typeck::BlockReturnType,
 };
 
-use crate::{cg_program_to_string, get_native_triple, stmt::cg_init_fn, CgScope};
+use crate::{stmt::cg_init_fn, CgScope};
 
 /// Initialize a new LLVM [`Builder`], [`FunctionValue`], [`CgScope`], and
 /// [`BasicBlock`] within that function. This allows you to generate code within
