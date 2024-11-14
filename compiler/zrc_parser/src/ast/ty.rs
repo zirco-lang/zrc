6218483406a12bcf41e8ba685c6650425520c1d8
0xLogN <nutdriver716@gmail.com>
2024-11-14 17:25:15 +0000

    chore: i missed one lol

--- compiler/zrc_parser/src/ast/ty.rs
+++ compiler/zrc_parser/src/ast/ty.rs
@@ -47,7 +47,7 @@ pub enum TypeKind<'input> {
     /// A direct union type
     Union(KeyTypeMapping<'input>),
 }
-impl<'input> Display for TypeKind<'input> {
+impl Display for TypeKind<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Identifier(i) => write!(f, "{i}"),
