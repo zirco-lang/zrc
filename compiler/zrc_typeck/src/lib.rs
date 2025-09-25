af46d1eeed16f7e0c9177ec1cdedffeb9d503ff0
0xLogN <nutdriver716@gmail.com>
2025-09-25 12:14:40 -0700

    chore: replace string_to_string with implicit_clone lint

--- compiler/zrc_typeck/src/lib.rs
+++ compiler/zrc_typeck/src/lib.rs
@@ -31,7 +31,7 @@
     clippy::same_name_method,
     clippy::semicolon_inside_block,
     clippy::unseparated_literal_suffix,
-    clippy::string_to_string,
+    clippy::implicit_clone,
     clippy::todo,
     clippy::undocumented_unsafe_blocks,
     clippy::unimplemented,
