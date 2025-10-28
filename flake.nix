ccb645189096072c88164607f773623f4df18b14
Logan Devine <nutdriver716@gmail.com>
2025-10-28 08:18:55 -0700

    repo: add Nix package

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    naersk.url = "github:nix-community/naersk";
   };
   
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, naersk }:
     flake-utils.lib.eachDefaultSystem(system:
       let
         overlays = [ rust-overlay.overlays.default ];
@@ -29,6 +30,30 @@
 
           LLVM_SYS_201_PREFIX = llvm.llvm.dev;
         };
+
+        packages.zrc =
+          let naerskLib = naersk.lib.${system}.override {
+            cargo = pkgs.rust-bin.stable.latest.default;
+            rustc = pkgs.rust-bin.stable.latest.default;
+          };
+          in naerskLib.buildPackage {
+            pname = "zrc";
+            src = ./.;
+            buildInputs = with pkgs; [
+              rust-bin.stable.latest.default
+              llvm.llvm
+              llvm.libllvm
+              llvm.clang
+              llvm.lld
+              pkg-config
+              libffi
+              libxml2
+            ];
+
+            LLVM_SYS_201_PREFIX = llvm.llvm.dev;
+         };
+
+        packages.default = self.packages.${system}.zrc;
       }
     );
 }
