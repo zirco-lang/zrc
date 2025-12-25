ecd6b99b4e3daadabca9595e1ebd4b8017d9bed4
0xLogN <nutdriver716@gmail.com>
2025-12-24 17:57:08 -0800

    repo: make examples depend on std

--- examples/common.mk
+++ examples/common.mk
@@ -9,12 +9,13 @@ ECHO := @echo
 MAKEFLAGS += --no-print-directory
 endif
 
+LIBZR_DIR ?= ../../libzr/dist/
 ZRC ?= ../../target/debug/zrc
 ZIRCOP ?= ../../target/debug/zircop
 ZRFLAGS ?= -I../../include/
 ZIRCOPFLAGS ?= -I../../include/
 CC ?= clang
-LDFLAGS ?= -lc
+LDFLAGS ?= -lc $(LIBZR_DIR)/libzr.a
 OUTDIR ?= ./out
 
 ifeq ($(shell uname),Darwin)
@@ -24,7 +25,14 @@ endif
 ZR_SOURCES ?= $(wildcard *.zr)
 ZR_OUTPUTS ?= $(ZR_SOURCES:%.zr=$(OUTDIR)/%.o)
 
-all: $(ZR_OUTPUTS)
+libzr: $(LIBZR_DIR)/libzr.a
+$(LIBZR_DIR)/libzr.a:
+	@echo "Please build libzr first in $(LIBZR_DIR)"
+	@exit 1
+
+all: $(OUTDIR)/run
+
+$(OUTDIR)/run: libzr $(ZR_OUTPUTS)
 	$(ECHO) "  CCLD   run"
 	$(Q)$(CC) $(CFLAGS) -o $(OUTDIR)/run $(ZR_OUTPUTS) $(LDFLAGS)
 
@@ -38,7 +46,7 @@ clean:
 	$(Q)rm -rf $(OUTDIR)
 
 .PHONY: test
-test: all
+test: $(OUTDIR)/run
 	$(ECHO) -n "  CHECK: "
 	$(Q)V=$V ../test_harness.sh $(OUTDIR)/run
 
