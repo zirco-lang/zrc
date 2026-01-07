8070c9a1b1982204b6bca06e7611beb8329225e9
Logan Devine <nutdriver716@gmail.com>
2026-01-07 13:32:47 -0800

    fix ICE from stdin path

--- compiler/zrc_diagnostics/src/diagnostic.rs
+++ compiler/zrc_diagnostics/src/diagnostic.rs
@@ -69,7 +69,7 @@ impl<K: Debug + PartialEq + Eq + Display> GenericDiagnostic<K> {
         // read the source from the path inside of the span. if it is <stdin>, use
         // the provided piped source.
         let (source, path) = match span.file_name() {
-            "<stdin>" => (
+            "/dev/<stdin>" => (
                 piped_source
                     .expect("piped source must be provided for <stdin>")
                     .to_string(),
