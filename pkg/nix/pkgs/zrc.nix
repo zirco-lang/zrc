bda9126a2ae01b91acbc8aa4f84e26ad0d828510
Logan Devine <logan@zirco.dev>
2026-09-02 20:55:54 -0700

    pkg/nix: fix RPATH for zlib and libstdc++

--- pkg/nix/pkgs/zrc.nix
+++ pkg/nix/pkgs/zrc.nix
@@ -1,6 +1,5 @@
 {
   naersk,
-  self,
   pkgs,
   llvm,
   rust,
@@ -8,12 +7,12 @@
   ...
 }:
 let
-  naerskLib = naersk.lib.${system}.override {
+  naersk' = naersk.lib.${system}.override {
     cargo = rust;
     rustc = rust;
   };
 in
-naerskLib.buildPackage {
+naersk'.buildPackage {
   name = "zrc";
   version = "0.2.0";
   src = ../../..;
@@ -32,19 +31,39 @@ naerskLib.buildPackage {
   ];
 
   env = {
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.stdenv.cc.cc.lib
-      llvm.libllvm
-      pkgs.libffi
-      pkgs.libxml2
-      pkgs.zlib
-    ];
+    LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (
+      with pkgs;
+      [
+        stdenv.cc.cc.lib
+        llvm.libllvm
+        libffi
+        libxml2
+        zlib
+      ]
+    )}";
   };
 
   postInstall = ''
     mkdir -p $out/include
     cp -r $src/include/* $out/include/
     cp $src/compiler/libzrc/zrc.h $out/include/
+  '';
+
+  postFixup = ''
+    # don't run the patchelf if we're on a dependency phase
+    if [ -e "$out/bin/zrc" ]; then
+      patchelf \
+        --set-rpath ${
+          pkgs.lib.makeLibraryPath (
+            with pkgs;
+            [
+              stdenv.cc.cc.lib
+              zlib
+            ]
+          )
+        } \
+        "$out/bin/"*
+    fi
   '';
 
   LLVM_SYS_221_PREFIX = llvm.llvm.dev;
